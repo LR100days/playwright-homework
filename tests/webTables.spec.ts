b5cffd4ee100ca39938e04f6274d752a36fb0344
Lidiia R <lidiia12rusakova@gmail.com>
2025-02-13 15:21:57 +0100

    Add tests related to vet speciality

--- tests/webTables.spec.ts
+++ tests/webTables.spec.ts
@@ -23,33 +23,42 @@ test.describe("Owners table practice", async () => {
 
         test('Validate search by Last Name', async ({page}) => {
             const lastNameInputField = page.getByRole('textbox')
-            const lastNames = ['Black', 'Davis', 'Es', 'Playwright']
+            const lastNamesForTest = ['Black', 'Davis', 'Es', 'Playwright']
 
-            for(let name of lastNames){
+            for(let lastname of lastNamesForTest){
                 await lastNameInputField.clear();
-                await lastNameInputField.fill(name);
+                await lastNameInputField.fill(lastname);
                 await page.getByRole('button', {name: 'Find Owner'}).click()
             
-                if(name == 'Playwright'){
+                if(lastname == 'Playwright'){
                     await expect(page.locator('.xd-container div').last()).toHaveText('No owners with LastName starting with "Playwright"')
                 } else {
-                    await page.waitForResponse(`https://petclinic-api.bondaracademy.com/petclinic/api/owners?lastName=${name}`);
-                    await expect(page.getByRole('row').locator('td').first()).toContainText(name)
+                    await page.waitForResponse(`https://petclinic-api.bondaracademy.com/petclinic/api/owners?lastName=${lastname}`);
+                    await expect(page.getByRole('row').locator('td').first()).toContainText(lastname)
                 }
             } 
         })
 
         test('Validate phone number and pet name on the Owner Information page', async ({page}) => {
-            // 1. Select the OWNERS menu item in the navigation bar and then select "Search" from the drop-down menu
-            // 2. Locate the owner by the phone number "6085552765". Extract the Pet name displayed in the table for the owner and save it to the variable. Click on this owner.
-            // 3. On the Owner Information page, add the assertion that "Telephone" value in the Onner Information card is "6085552765"
-            // 4. Add the assertion that Pet Name in the Owner Information card matches the name extracted from the page on the step 2
+            const ownerForTest = page.getByRole('row', {name: "6085552765"})
+            const petName = await ownerForTest.locator('td').last().textContent()
+            
+            await ownerForTest.getByRole('link').click()
+            await expect(page.getByRole('row', {name: "Telephone"})).toContainText('6085552765')
+            await expect(page.getByRole('table').locator("dd").first()).toHaveText(petName)
         })
 
         test('Validate pets of the Madison city', async ({page}) => {
-            // 1. Select the OWNERS menu item in the navigation bar and then select "Search" from the drop-down menu
-            // 2. On the Owners page, perform the assertion that Madison city has a list of pets: Leo, George, Mulligan, Freddy
-            // Hint: create an empty array. loop through the list of the rows, extract the value of the pet for each row that has Madison city, add the value of the pet to the array, perform the assertion that the array has the expected values
+            const madisonCityRows = page.locator("tbody tr").filter({hasText: "Madison"})
+            await expect(madisonCityRows).toHaveCount(4);
+            let pets: string[] = [];
+
+            for(let row of await madisonCityRows.all()){
+                let petName = await row.locator('td').last().innerText()
+                pets.push(petName.trim())
+            }
+            
+            expect(pets).toEqual(['Leo', 'George', 'Mulligan', 'Freddy'])
         })
 
 })
@@ -58,33 +67,109 @@ test('Validate specialty update', async ({page}) => {
     await page.getByRole('button', {name:"Veterinarians"}).click();
     await page.getByRole('link', {name:"All"}).click();
 
-    // 1. Select the VETERINARIANS menu item in the navigation bar, then select "All"
-    // 2. On the Veterinarians page, add the assertion that "Rafael Ortega" has specialty "surgery"
-    // 3. Select the SPECIALTIES menu item in the navigation bar
-    // 4. Add assertion of the "Specialties" header displayed above the table
-    // 5. Click on "Edit" button for the "surgery" specialty
-    // 6. Add assertion "Edit Specialty" page is displayed
-    // 7. Update the specialty from "surgery" to "dermatology" and click "Update button"
-    // 8. Add assertion that "surgery" was changed to "dermatology" in the list of specialties
-    // 9. Select the VETERINARIANS menu item in the navigation bar, then select "All"
-    // 10. On the Veterinarians page, add assertion that "Rafael Ortega" has specialty "dermatology"
-    // 11. Navigate to SPECIALTIES page, revert the changes renaming "dermatology" back to "surgery"
+// Verify initial target Veterinarian speciality
+    const targetRow = page.getByRole('row', {name:"Rafael Ortega"})
+    const targetSpeciality = targetRow.locator('td').nth(1)
+    await expect(targetSpeciality).toHaveText("surgery")
+
+// Navigate to the Specialties list
+    await page.getByRole('link', {name:'Specialties'}).click()
+    await expect(page.getByRole('heading')).toHaveText('Specialties')
+
+// Edit speciality from 'surgery' to 'dermatology' in the list of specialties
+    const targetSpecialityRow = page.getByRole('row').nth(2)
+    await targetSpecialityRow.getByRole('button', { name: 'Edit' }).click()
+
+    await expect(page.getByRole('heading')).toHaveText('Edit Specialty')
+
+    const specialityInputField = page.getByRole('textbox')
+    await specialityInputField.click()
+    await specialityInputField.clear()
+    await specialityInputField.fill('dermatology')
+    await page.getByRole('button', { name: 'Update' }).click()
+
+// Verify that speciality is updated in the Speciality list and for the target Veterinarian
+
+    expect(await targetSpecialityRow.locator('td input').inputValue()).toEqual('dermatology')
+
+    await page.getByRole('button', {name:"Veterinarians"}).click();
+    await page.getByRole('link', {name:"All"}).click();
+
+    await expect(targetSpeciality).toHaveText("dermatology")
+    
+// Navigate to SPECIALTIES page, revert the changes renaming "dermatology" back to "surgery"
+    await page.getByRole('link', {name:'Specialties'}).click()
+    await expect(page.getByRole('heading')).toHaveText('Specialties')
+
+    await targetSpecialityRow.getByRole('button', { name: 'Edit' }).click()
+    await specialityInputField.click()
+    await specialityInputField.clear()
+    await specialityInputField.fill('surgery')
+    await page.getByRole('button', { name: 'Update' }).click()
+
+    expect(await targetSpecialityRow.locator('td input').inputValue()).toEqual('surgery')
+
+    await page.getByRole('button', {name:"Veterinarians"}).click();
+    await page.getByRole('link', {name:"All"}).click();
+
+    await expect(targetSpeciality).toHaveText("surgery")
 
 })
 
 test('Validate specialty lists', async ({page}) => {
     await page.getByRole('link', {name:"Specialties"}).click();
-    // 1. Select the SPECIALTIES menu item in the navigation bar
-    // 2. On the Specialties page, select "Add" button. Type the new specialty "oncology" and click "Save" button
-    // 3. Extract all values of specialties and put them into the array.
-    // 4. Select the VETERINARIANS menu item in the navigation bar, then select "All"
-    // 5. On the Veterinarians page, locate the "Sharon Jenkins" in the list and click "Edit" button
-    // 6. Click on the Specialties drop-down menu. Extract all values from the drop-down menu to an array
-    // 7. Add the assertion that array of specialties collected in the step 3 is equal the the array from drop-down menu
-    // 8. Select the "oncology" specialty and click "Save vet" button
-    // 9. On the Veterinarians page, add assertion, that "Sharon Jenkins" has specialty "oncology"
-    // 10. Navigate to SPECIALTIES page. Click "Delete" for "oncology" specialty
-    // 11. Navigate to VETERINARIANS page. Add assertion that "Sharon Jenkins" has no specialty assigned
-    // Hint: For step 3, create an empty array, then loop through the list of the table rows, getting the value for each row and adding to the array
+    await page.getByRole('button', {name:"Add"}).click();
+
+    const addSpecialityField = page.getByRole('textbox').last()
+    await addSpecialityField.click()
+    await addSpecialityField.fill('oncology')
+    await page.getByRole('button', {name:"Save"}).click();
+
+    await page.waitForResponse('https://petclinic-api.bondaracademy.com/petclinic/api/specialties')
+
+// Extract all specialities in the list
+    const allRows = page.getByRole('row').filter({has: page.getByRole('button', {name:"Edit"})})
+    let allSpecialities: string[] = [];
+
+    for(let row of await allRows.all()){
+        let spec = await row.locator('td input').inputValue()
+        allSpecialities.push(spec)
+    }
+
+// Edit speciality for target veterinarian (targetVet)
+    await page.getByRole('button', {name:"Veterinarians"}).click();
+    await page.getByRole('link', {name:"All"}).click();
+   
+    const targetVetRow = page.getByRole('row', {name:"Sharon Jenkins"})
+    await targetVetRow.getByRole('button', {name:"Edit"}).click();
+  
+    await page.locator('.dropdown-display').click()
+
+    const dropdownOptions = page.locator('.dropdown-content div label')
+    let specialitiesOptions: string[] = []
+
+    for(let option of await dropdownOptions.all()){
+        let optionValue = await option.innerText()
+        specialitiesOptions.push(optionValue)
+    }
+    
+    expect(specialitiesOptions).toEqual(allSpecialities)
+   
+    await page.getByRole('checkbox', {name: 'oncology'}).check()
+    expect(await page.getByRole('checkbox', {name: 'oncology'}).isChecked()).toBeTruthy
+    await page.locator('.dropdown-display').click() // to close the dropdown
+    await page.getByRole('button', {name:"Save Vet"}).click();
+
+// Check updated speciality for target Vet
+    const specialitysharonJenkins = targetVetRow.locator('td').nth(1)
+    await expect(specialitysharonJenkins).toHaveText('oncology')
+
+// Delete newly added speciality from the Specialitis list and verify that deleted speciality is also removed for targetVet 
+    await page.getByRole('link', {name:"Specialties"}).click();
+    await page.getByRole('row', {name: 'oncology'}).getByRole('button', {name:"Delete"}).click()
+    await page.getByRole('button', {name:"Veterinarians"}).click();
+    await page.getByRole('link', {name:"All"}).click();
+
+    await expect(specialitysharonJenkins).toBeEmpty()
 
 })
