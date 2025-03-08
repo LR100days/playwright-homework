0c251021b1725cbbf0ed9f647d79eecc68945ff1
Lidiia R <lidiia12rusakova@gmail.com>
2025-03-08 11:50:07 +0100

    API mocking draft

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -3,17 +3,31 @@ import { test, expect } from '@playwright/test';
 import ownersDetails from '../test-data/ownersDetails.json'
 
 test.beforeEach( async({page}) => {
-  await page.route('https://petclinic-api.bondaracademy.com/petclinic/api/owners', async route => {
+  await page.route('*/**/api/owners', async route => {
     await route.fulfill({
         body: JSON.stringify(ownersDetails)
     })
   })
 
-//   const pm = new PageManager(page)
-//   await pm.navigateTo().ownersPage()
+  await page.route(`*/**/api/owners/${ownersDetails[0].id}`, async route => {
+
+    await route.fulfill({
+      body: JSON.stringify(ownersDetails[0])
+    })
+  })
+
+  await page.goto("/owners");
+  //const pm = new PageManager(page)
+  //await pm.navigateTo().ownersPage()
 
 })
 
 test('mocking API request', async ({page}) => {
-    await expect
+  await expect(page.locator('.ownerFullName')).toHaveCount(2)
+  await page.getByRole('link', { name: 'Adam Braun' }).click()
+  await expect(page.locator('.ownerFullName')).toHaveText('Adam Braun')
+  
+  await expect(page.locator('table').first().locator('tr td').last()).toContainText('3456789012')
+  await expect(page.locator('.dl-horizontal dd').first()).toContainText(`${ownersDetails[0].pets[0].name}`)
+
 });
